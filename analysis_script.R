07def2c80437e987bfd4258fbd5b3d446b66f4c7
Fatya <fatya@internationalshowtimes.com>
2025-11-21 23:07:56 +0100

    Sorted cities from lowest to highest rent-to-income ratio

--- analysis_script.R
+++ analysis_script.R
@@ -4,16 +4,18 @@ library(readxl)
 library(dplyr)
 # Reading the imported .xlsx file with the dataset into a dataframe 
 housing_data <- read_excel("Housing Affordability Data Collection.xlsx")
-# Cleaning the data: renaming columns and data values. Sorting.
+# Cleaning the data: Renaming columns to standardized, clean names.
 housing_data_stacked <- housing_data %>%
   rename(Rent_Amount = Rent_1_Amount,
          Source_Rent = Source_Rent_1,
          rent_to_income_ratio = `Rent to Income ratio`) %>% 
+#Clean up city names  
   mutate(City =case_when(City == "Grande Lisboa" ~ "Lisbon",
                          City == "Comunidad de Madrid" ~ "Madrid",
-                         City == "Warszawski stoĹeczny" ~ "Warsaw",TRUE ~ City),
-         # Calculating the Disposable Income
-         Disposable_Income = Monthly_Net_Income_EUR - Rent_Amount)
+                         City == "Warszawski stoĹeczny" ~ "Warsaw",TRUE ~ City))
+# Calculating the Disposable Income
+housing_data_stacked <- housing_data_stacked %>%
+  mutate(Disposable_Income = Monthly_Net_Income_EUR-Rent_Amount)
 head(housing_data_stacked)
 # Transforming the data from wide to long
 housing_data_long_stacked <- housing_data_stacked %>%
@@ -31,7 +33,7 @@ housing_plot <- ggplot(data = housing_data_long_stacked)+
             fontface = "bold",
             size = 4
   ) + 
-  # Defining labels (title, axes, footnote)
+  # Defining labels
   labs(title = "Rental Affordability in 5 Major EU capitals: Rent as % of Monthly Net Income",
        subtitle = "The bar height shows Total Monthly Net Income. The red segment stands for Monthly Rent.",
        x = "City",
